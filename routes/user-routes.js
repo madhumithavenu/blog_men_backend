e805ee323b88510642dc2a02e607f187493cc87e
Madhumitha <madhumithavenu03@gmail.com>
2023-09-30 12:25:58 +0530

    Adding Signup details

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const getAllUser = require("../controllers/user-controller.js");
+const signup = require("../controllers/user-controller.js");
 
 const router = express.Router();
 
 
 router.get("/", getAllUser);
+router.post("/signup",signup);
 
 module.exports = router;
