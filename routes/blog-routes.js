4005fc129a69bac7f7efcf44f2ad1340c80745dc
Madhumitha <madhumithavenu03@gmail.com>
2023-09-30 15:38:12 +0530

    getUserby id

--- routes/blog-routes.js
+++ routes/blog-routes.js
@@ -10,8 +10,11 @@ const blogRouter = express.Router();
 
 blogRouter.get("/", getAllBlogs);
 blogRouter.post("/add", addBlog);
+
 blogRouter.put("/update/:id", updateBlog);
+
 blogRouter.get("/:id", getById);
+
 blogRouter.delete("/:id", deleteBlog);
 blogRouter.get("/user/:id", getByUserId);
 
